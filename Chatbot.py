ee397985e174fb9a8e0a4e80974aded6804d1107
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-09-25 12:35:24 -0700

    added more code

--- Chatbot.py
+++ Chatbot.py
@@ -1,4 +1,17 @@
 # Chatbot
 # Author: Illia Nyshpor
-# Date: 21.09.23
+# Date: 21.09.23 / 25.09.23
+
+#Greet the user
+#Get the users name, store it in a variable
+#Respond with the users name
+#Ask for their fav food
+#Respond with something appropriate
+
+print("Hi!")
+print("I am a crude chatbot. You can talk to me")
+uname = input("What`s your name?")
+print(f"Nice to meet you, {uname}")
+ff = input(f"What is your favourite food {uname}?")
+print(f"You have a weird taste. {ff} is disgusting!")
 
