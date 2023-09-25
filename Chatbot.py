2dcbf709a8161fdc7c17c4266c0a65fadac2d36e
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-09-25 12:48:13 -0700

    more code

--- Chatbot.py
+++ Chatbot.py
@@ -8,10 +8,26 @@
 #Ask for their fav food
 #Respond with something appropriate
 
+#Respond with something not too repetive
+#Create a list of responces
+#Chose one random from the list
+#Print chosen responce
+
 print("Hi!")
 print("I am a crude chatbot. You can talk to me")
 uname = input("What`s your name?")
 print(f"Nice to meet you, {uname}")
 ff = input(f"What is your favourite food {uname}?")
-print(f"You have a weird taste. {ff} is disgusting!")
 
+list_of_ff_resp = [
+    f"You have a weird taste. {ff} is disgusting",
+    "Sounds tasty",
+    f"I also kinda like {ff}",
+    "Nice",
+    "Cool, i also like it!",
+    "I approve of your choice"
+]
+
+import random
+rr = random.choice(list_of_ff_resp)
+print(rr)
