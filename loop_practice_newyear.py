ce40d107a29f173754549e54ec4495647ed32d36
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-10-12 13:31:00 -0700

    Finished new year loop practice

---
+++ loop_practice_newyear.py
@@ -0,0 +1,15 @@
+# loop practice
+# Illia Nyshpor
+# Date: 10.10.23
+
+# Create a new years countdown
+# Starts at ten and counts down to zero
+# Print happy new year
+
+numb = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1,] 
+
+import time
+for im in numb:
+     time.sleep(1)
+     print(im)
+print("It is not New Year yet, calm down!")
