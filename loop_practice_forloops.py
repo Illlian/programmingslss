e95be100cdc33ddda67c38ab109de6630f894705
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-10-17 10:48:26 -0700

    loop practice specifically for for loops

---
+++ loop_practice_forloops.py
@@ -0,0 +1,24 @@
+# loop practice
+# Illia Nyshpor
+# Date: 10-13.10.23
+
+for _ in range(10):
+ print("Something random")
+
+for i in range (5):
+    print(i)
+
+
+for i in range(3, 100):
+   print(i)
+            
+for i in range(1200, 1501, 2):
+   print(i)             
+
+# Activity
+#1 
+for i in range(1200, 1501, 2):
+    print(i)
+#2
+for i in range(-149, 0, 2):
+    print(i)
