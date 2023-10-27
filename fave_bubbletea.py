9edacca7a70da5fe7c3160337791cff76aff419c
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-10-27 12:50:01 -0700

    bubble tea bot started

---
+++ fave_bubbletea.py
@@ -0,0 +1,30 @@
+# Bubble tea popularity algoritm
+# illia Nyshpor
+# Date: 27.10.23
+
+# ask 5 users about thie favourite bubble tea place and print out the results
+
+# Ast the user about their favourite bubble tea place
+
+# Counting algoritm
+# Options: Coco Chatime SunTea Xing Fu Tang Bubble Queeen
+# If they say one of the options the number increases by one
+# Print out the results
+
+print("What is your favourite bubble tea store? ")
+fb = input().strip(",.?! ").lower
+
+coco_likes = 0
+Chatime_likes = 0
+suntea_likes = 0
+Xian_Fu_Tang_likes = 0
+bbl_queen = 0
+
+if fb == "coco":
+    coco_likes = coco_likes +1
+elif fb == "suntea":
+    suntea_likes += 1
+
+print()
+ 
+
