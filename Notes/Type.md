31395241d6e5a18abe6752df8c6ed20a5b18339e
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-10-30 12:50:40 -0700

    new code +notes

--- Notes/Type.md
+++ Notes/Type.md
@@ -16,3 +16,17 @@ age = 16
 age_float = 16.0
 # age_float = type of float
 ```
+
+
+## Converting types
+There are some **special** functions that help to convert data from one type to another.
+
+```python
+intro_string = "my_age"
+my_age = 16
+
+"My name is" + "random name"
+"my name is" + " " + "random name"
+
+strinng + my_age
+```
--- fave_bubbletea.py
+++ fave_bubbletea.py
@@ -1,6 +1,6 @@
 # Bubble tea popularity algoritm
 # illia Nyshpor
-# Date: 27.10.23
+# Date: 27.10.23 / 30.10.23
 
 # ask 5 users about thie favourite bubble tea place and print out the results
 
@@ -10,21 +10,40 @@
 # Options: Coco Chatime SunTea Xing Fu Tang Bubble Queeen
 # If they say one of the options the number increases by one
 # Print out the results
+ 
+NUM_RESPONDDENTS = 2
 
-print("What is your favourite bubble tea store? ")
-fb = input().strip(",.?! ").lower
-
-coco_likes = 0
+coco_likes = 0  
 Chatime_likes = 0
 suntea_likes = 0
 Xian_Fu_Tang_likes = 0
-bbl_queen = 0
+bbqueen = 0
 
-if fb == "coco":
-    coco_likes = coco_likes +1
-elif fb == "suntea":
-    suntea_likes += 1
+for _ in range(NUM_RESPONDDENTS):
 
-print()
+    print("What is your favourite bubble tea store? ")
+    fb = input().strip(",.?! ").lower
+
+    if fb == "coco":
+        coco_likes = coco_likes +1
+    elif fb == "Chatime":
+        Chatime_likes += 1
+    elif fb == "suntea":
+        suntea_likes += 1
+    elif fb == "Xing Fu Tang":
+        Xian_Fu_Tang_likes += 1
+    elif fb == "Bubble Queeen":
+        bbqueen =+ 1
+
+
+print(f"Coco likes: {coco_likes}")
+      
+print (f"Chatime likes: {Chatime_likes}")
+
+print(f"Suntea likes: {suntea_likes}")
+
+print(f"Xian Fu Tang likes: {Xian_Fu_Tang_likes}") 
+
+print(f"Bubble Queeen: {bbqueen}")
  
 
