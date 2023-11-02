c6027ddbfe5dd14f70f28f642ed6792e5729b754
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-11-02 14:49:17 -0700

    numbers practice questions

---
+++ age_in_2049.py
@@ -0,0 +1,13 @@
+# Question 1
+# illia Nyshpor
+# Date: 02.11.2
+
+print("I will heplp you find out how old you will be in 2049")
+print("What year it is right now? ")
+year = int(input())
+print("how old are you? ")
+age = int(input())
+
+fa = 2049 - year + age
+
+print(f"In 2049 you will be {fa} years old")
