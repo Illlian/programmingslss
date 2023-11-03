e46acaff5ff5e32228e6ac32e081a0ab05ae259f
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-11-03 12:47:47 -0700

    started new problems

---
+++ world_traveller.py
@@ -0,0 +1,16 @@
+# World travel bot
+# Illia Nyshpor
+# Date: 03.11.23
+
+name = input("Hi, what is your name? ")
+print(f"Welcome {name}, i just wnted to ask about your travelling history")
+
+continents = ["Have you ever been to Asia",
+              "Have you ever been to Europe",
+              "Have you ever been to North America",
+              "Have you ever been to South America",
+              "Have you ever been to Asia",
+              "Have you ever been to Asia",
+              "Have you ever been to Asia",
+              "Have you ever been to Asia",
+              ]
