6d2cfbb48f7c1e615aa5b2024995a443b239c86c
Illian <sf.nyshpor@gmail.com>
2023-11-04 18:48:01 -0700

    updated the question code

--- mcdolands.py
+++ mcdolands.py
@@ -6,11 +6,11 @@ print("I am here to take your order!")
 
 total_price = 0
 
-burger = input("Would you like a burger for $5? (yes/no) ")
-if burger == "yes" or burger == "Yes":
+burger = input("Would you like a burger for $5? (yes/no) ").strip(",.?").lower()
+if burger == "yes":
     total_price += 5
-fries = input("Would you like some fries for $3? (yes/no) ")
-if fries == "yes" or fries == "Yes":
+fries = input("Would you like some fries for $3? (yes/no) ").strip(",.?").lower()
+if fries == "yes":
     total_price += 3
 else:
     print("Sorry we could not help you! Thank you for visiting McDolands!")
