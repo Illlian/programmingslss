01266a45c9b6a0c0e01b12243108253660cebe29
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-11-06 12:50:29 -0800

    working on new problems

--- semester_evaluator.py
+++ semester_evaluator.py
@@ -1,3 +1,21 @@
 # Semester evaluator bot
 # Illia Nyshpor
-# Date: 03.11.23
+# Date: 06.11.23
+
+print("Hi, I am here to give a rating to your semester")
+cources = int(input("How many cources are you taking this semester? "))
+
+rating_cources = 0
+num_cources = 0
+
+for i in range (cources):
+    rating = float(input(f"How do you rate cource {num_cources +1} out of 5? "))
+    rating_cources += rating / cources
+
+
+if rating_cources < 1:
+    print(f"Your overall score is {rating_cources :.1f}, Ouch")
+elif 3 > rating_cources > 1:
+    print(f"Your overall score is {rating_cources:.1f}, Not a bad semester")
+elif rating_cources > 3:
+    print(f"Your overall score is {rating_cources:.1f}, Glad to hear that")
--- world_traveller.py
+++ world_traveller.py
@@ -1,16 +1,30 @@
 # World travel bot
 # Illia Nyshpor
-# Date: 03.11.23
+# Date: 03.11.23 / 06.11.23
 
 name = input("Hi, what is your name? ")
 print(f"Welcome {name}, i just wnted to ask about your travelling history")
 
-continents = ["Have you ever been to Asia",
+continents = ["Have you ever been to Asia?",
               "Have you ever been to Europe",
-              "Have you ever been to North America",
-              "Have you ever been to South America",
-              "Have you ever been to Asia",
-              "Have you ever been to Asia",
-              "Have you ever been to Asia",
-              "Have you ever been to Asia",
+              "Have you ever been to North America?",
+              "Have you ever been to South America?",
+              "Have you ever been to Australia?",
+              "Have you ever been to Africa?",
+              "Have you ever been to Antarctica?"
               ]
+
+num_continents = 0
+
+for continent in continents:
+
+    print(continent)
+    total = input().lower
+
+    if total == "yes":
+      num_continents +=1
+      print(num_continents)
+        
+print(f"Cool, you have visites {num_continents}/7 continents")
+
+
