45a9f03d8537fd2c988894f0342bfae4b0a5c98d
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-11-07 10:22:31 -0800

    finished code

--- semester_evaluator.py
+++ semester_evaluator.py
@@ -2,14 +2,14 @@
 # Illia Nyshpor
 # Date: 06.11.23
 
-print("Hi, I am here to give a rating to your semester")
+print("Hi, I am here to give a rating to your semester.")
 cources = int(input("How many cources are you taking this semester? "))
 
 rating_cources = 0
 num_cources = 0
 
 for i in range (cources):
-    rating = float(input(f"How do you rate cource {num_cources +1} out of 5? "))
+    rating = float(input("How do you rate cource out of 5? "))
     rating_cources += rating / cources
 
 
