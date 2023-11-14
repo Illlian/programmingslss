fc9f50ff284e4dd0890621cc7b6b1a2212a1c8f2
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-11-14 12:37:29 -0800

    started practice question

---
+++ similar_hobbies.py
@@ -0,0 +1,11 @@
+# Similarity score practice 
+# Question 1
+# Illia Nyshpor
+# Date: 14.11.23
+
+first_hobbies = input(" What are your hobbies? ").lower().strip
+second_hobbies = input(" What are your hobbies? ").lower().strip
+
+ss = 0
+
+
