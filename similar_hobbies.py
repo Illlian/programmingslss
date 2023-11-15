ac9de859ee7dcde8f3f8d7e4b9ba430e45089c55
Illian <sf.nyshpor@gmail.com>
2023-11-14 21:32:22 -0800

    Added new code

--- similar_hobbies.py
+++ similar_hobbies.py
@@ -3,9 +3,24 @@
 # Illia Nyshpor
 # Date: 14.11.23
 
-first_hobbies = input(" What are your hobbies? ").lower().strip
-second_hobbies = input(" What are your hobbies? ").lower().strip
+
+first_hobbies = input(" What are your hobbies? ").lower().split()
+second_hobbies = input(" What are your hobbies? ").lower().split()
 
 ss = 0
+# def double(bonus):
+#     return bonus.strip(".,? ")
+
+# hobbies = map(double, first_hobbies)
+# hobbies_two = map(double, second_hobbies)
 
 
+
+
+
+for hobbie in first_hobbies:
+    if hobbie in second_hobbies:
+        ss += 1
+        
+
+print(f"You have {ss} hobbies in common")
