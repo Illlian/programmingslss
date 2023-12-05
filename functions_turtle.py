c062a44511469a7369f28c502b546333d4589060
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-12-05 12:50:10 -0800

    rwcursion code

--- functions_turtle.py
+++ functions_turtle.py
@@ -52,6 +52,42 @@ lewis.width(4)
 lewis.color("brown")
 lewis.shape("arrow")
 
-draw_tree(12, 150)
+
+
+
+def draw_fancy_tree(level: int, height: int) -> None:
+    """A recursion function that draws a tree with the initial given height
+      """
+    
+    if level > 0:
+        lewis.forward(height)
+
+        #lewis.forward(39)
+        draw_fancy_tree(level - 1, height / 1.5)
+        
+        lewis.left(39)
+        draw_fancy_tree(level - 1, height / 1.5)
+        
+        lewis.right(39*2)
+        draw_fancy_tree(level - 1, height / 1.5)
+        lewis.left(39)
+        
+
+        lewis.back(height)
+
+    else:
+        oc = lewis.color()
+        lewis.color("green")
+        lewis.stamp()
+        lewis.color(oc[0])
+
+
+lewis.setheading(90)
+lewis.speed(0)
+lewis.width(4)
+lewis.color("brown")
+lewis.shape("arrow")
+
+draw_fancy_tree(4, 150)
 
 turtle.done()
