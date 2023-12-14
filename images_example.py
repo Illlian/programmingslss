890ab022635897ca67e3831386411a910870e2a0
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-12-14 12:13:19 -0800

    images code

--- images_example.py
+++ images_example.py
@@ -4,17 +4,32 @@
 
 from PIL import Image 
 
-with Image.open("./Images/kid-green.jpg") as im:
+def pixel_to_name(pixel: tuple) -> str:
+    red, green, blue = pixel 
 
+    if red < 55 and blue < 55 and green > 140:
+        return "green"
+    else:
+        return "colour unknown"
+
+with Image.open("./Images/kid-green.jpg") as im:
     ih = im.height
     iw = im.width
 
+    bg_im  = Image.open("./Images/beach.jpg")
     for y in range(ih):
         for x in range(iw):
 
             pixel = im.getpixel((x,y))
-            print(x, y, pixel)
+     
+            if pixel_to_name(pixel) == "green":
+                bg_pixel = bg_im.getpixel((x, y))
 
+                im.putpixel((x, y), bg_pixel)
+
+bg_im.close()
+
+im.save("./Images/output.jpg")
 
 
 # Image practice example
