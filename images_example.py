4fbf93479ce63699e54ff59b3ea69de7fb8babff
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-12-14 11:26:06 -0800

    adde new images exercise

--- images_example.py
+++ images_example.py
@@ -1,11 +1,29 @@
 # Images and python
 # Illia Nyshpor
-# 11.12.23
+# 11.12.23 / 14.12.23
 
 from PIL import Image 
 
 with Image.open("./Images/kid-green.jpg") as im:
 
-    pixel = im.getpixel((0,0))
+    ih = im.height
+    iw = im.width
 
-    print(pixel)
+    for y in range(ih):
+        for x in range(iw):
+
+            pixel = im.getpixel((x,y))
+            print(x, y, pixel)
+
+
+
+# Image practice example
+# from PIL import Image 
+# with Image.open("./Images/kid-green.jpg") as im:
+#    pixel = im.getpixel((0,0))
+#    print(pixel)
+
+#   mc = im.width // 2
+#   pixel = im.getpixel((mc, mc))
+#   print(pixel)
+
