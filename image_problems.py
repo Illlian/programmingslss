6ff2215b30edbe676c2bef348b5c060b1cd88f93
Illia Nyshpor <sf.nyshpor@gmail.com>
2023-12-19 12:15:29 -0800

    new image code to edit

--- image_problems.py
+++ image_problems.py
@@ -45,3 +45,45 @@ with Image.open("./Images/catto.png") as im:
                 im.putpixel((x,y), dark_pixel)
 
 im.save("./Images/output.jpg")
+
+im.close()
+
+
+
+
+# Question 3
+def pixel_to_grayscale(pixel: tuple) -> tuple:
+    """A function that converts an image into a gray scale"""
+
+    red, green, blue = pixel
+
+    avg = (red + green + blue) / 3 
+
+    return avg
+
+
+with Image.open("./Images/catto.png") as im:
+        ih = im.height
+        iw = im.width
+
+        for y in range(ih):
+            for x in range(iw):
+
+                pixel = im.getpixel((x,y))
+
+                if pixel_to_grayscale(pixel) > 128:
+                    im.putpixel((x,y), dark_pixel)
+
+                elif pixel_to_grayscale(pixel) < 127:
+                    im.putpixel((x,y), light_pixel)
+
+                elif 127 < pixel_to_grayscale(pixel) <= 128:
+                    im.putpixel((x,y), dark_gray)
+                    
+                else:
+                    im.putpixel((x,y), light_gray)
+
+
+                im.save("./Images/output.jpg")               
+
+
