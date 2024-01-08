49cb48f0aec07cb8a4e8297e706521122a8fb504
Illian <sf.nyshpor@gmail.com>
2024-01-07 20:46:46 -0800

    additional changes to code

--- image_problems.py
+++ image_problems.py
@@ -5,16 +5,7 @@
 # Question 1
 from PIL import Image
 
-def is_light(pixel: tuple) -> bool:
-    """A fuction that determines 
-    weather a pixel is light or dark"""
-
-    red, green, blue = pixel
-
-    if (red + green + blue) / 3 >= 128:
-        return True
-    else:
-        return False
+import colour_helper
 
 # Test
 light_pixel = (255, 255, 255)
@@ -22,14 +13,15 @@ light_gray = (128, 128, 128)
 dark_gray = (127, 127, 127)
 dark_pixel = (0, 0, 0)
 
-print(is_light(light_pixel))  # True
-print(is_light(light_gray))  # True
-print(is_light(dark_gray))  # False
-print(is_light(dark_pixel))  # False
+print(colour_helper.is_light(colour_helper.light_pixel))  # True
+print(colour_helper.is_light(colour_helper.light_gray))  # True
+print(colour_helper.is_light(colour_helper.dark_gray))  # False
+print(colour_helper.is_light(colour_helper.dark_pixel))  # False
 
 
 
-# Question 2
+# Question 1
+# Change image to black and white
 with Image.open("./Images/catto.png") as im:
      ih = im.height
      iw = im.width
@@ -39,10 +31,10 @@ with Image.open("./Images/catto.png") as im:
 
             pixel = im.getpixel((x,y))
      
-            if is_light(pixel) == True:
-                im.putpixel((x,y), light_pixel)
+            if colour_helper.is_light(pixel) == True:
+                im.putpixel((x,y), colour_helper.light_pixel)
             else:
-                im.putpixel((x,y), dark_pixel)
+                im.putpixel((x,y), colour_helper.dark_pixel)
 
 im.save("./Images/output.jpg")
 
@@ -50,40 +42,28 @@ im.close()
 
 
 
+# Question 2
+# Change image into gray
+def picture_to_grayscale(filename: str) -> None:
+    """Convert a given picture to grayscale"""
 
-# Question 3
-def pixel_to_grayscale(pixel: tuple) -> tuple:
-    """A function that converts an image into a gray scale"""
-
-    red, green, blue = pixel
-
-    avg = (red + green + blue) / 3 
-
-    return avg
-
-
-with Image.open("./Images/catto.png") as im:
+    # Open up the image
+    with Image.open(f"./Images/{filename}") as im:
+        # Visit every pixel
         ih = im.height
         iw = im.width
 
         for y in range(ih):
             for x in range(iw):
 
-                pixel = im.getpixel((x,y))
+                pixel = im.getpixel((x, y))
+              
+                # Take that pixel and convert it to gray
+                gray_pixel = colour_helper.pixel_to_grayscale(pixel)
 
-                if pixel_to_grayscale(pixel) > 128:
-                    im.putpixel((x,y), dark_pixel)
-
-                elif pixel_to_grayscale(pixel) < 127:
-                    im.putpixel((x,y), light_pixel)
-
-                elif 127 < pixel_to_grayscale(pixel) <= 128:
-                    im.putpixel((x,y), dark_gray)
-                    
-                else:
-                    im.putpixel((x,y), light_gray)
-
-
-                im.save("./Images/output.jpg")               
-
-
+                im.putpixel((x, y), gray_pixel)
+        
+        # Save the image
+        im.save("./Images/grayscale.jpg")
+        
+picture_to_grayscale("catto.png")
