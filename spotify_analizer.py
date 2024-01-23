9fc5cab3ec7c88992907261de9de13f3d24d9d33
Illia Nyshpor <sf.nyshpor@gmail.com>
2024-01-23 11:33:10 -0800

    slightly edited the code

--- spotify_analizer.py
+++ spotify_analizer.py
@@ -11,14 +11,14 @@ def find_all_songs(artists: str) -> list:
         csv_r = csv.DictReader(f)
 
         # ds = []
-        s = []
+        songs = []
         for line in csv_r:
             # print(line)
             if artists.lower() in line["artist"].lower():
-                s.append(
+                songs.append(
                    (line["artist"], line["song_title"], line["danceability"])
                 )
-        return s
+        return songs
        
 drake_songs = find_all_songs("Drake")
 # for s in drake_songs:
@@ -28,19 +28,17 @@ drake_songs = find_all_songs("Drake")
 
 # Version 0.1
 # Get all the songs by Drake from the data list
-with open("./spotify.csv") as f:
-    # ---- Look for all songs performed by Drake
+# ---- Look for all songs performed by Drake
     #      Use linear search
     # Create a csv reader object
+
+with open("./spotify.csv") as f:
     csv_reader = csv.DictReader(f)
 
-    # Create a list to store all Drake's songs
     drake_songs = []
 
-    # for every song in the .csv file
     for line in csv_reader:
         if "drake" in line["artist"].lower():
-            # add it to the Drake's songs list
             drake_songs.append(
                 (line["artist"], line["song_title"], line["danceability"])
             )
@@ -53,15 +51,14 @@ with open("./spotify.csv") as f:
 # --- Sorting Algorithm (lowest to highest)
 # --- Selection Sort
 
-# Starting at the beginning of the list moving to the end
+
 for i in range(len(drake_songs)):
     # Set the current item to the smallest value
     smallest_danceability = drake_songs[i][-1]
     smallest_index = i
 
-    # For the rest of the list
     for j in range(i + 1, len(drake_songs)):
-        # Check to see if this is smaller
+      
         if drake_songs[j][-1] < smallest_danceability:
             smallest_danceability = drake_songs[j][-1]
             smallest_index = j
@@ -80,17 +77,9 @@ for song in drake_songs:
  # Task #2
  # Print all the songs that have danceability >= 0.6
 
-    # with open("./spotify.csv") as f:
-    #     csv_r = csv.DictReader(f)
-
-    #     dnc = []
-
-    #     for line in csv_r:
-    #         # Alt version: 
-    #         # if float(song[-1]) >= 0.6
-    #         if float(line["danceability"]) >= 0.6:
-    #             dnc.append(
-    #                 (line["artist"], line["song_title"], line["danceability"])   
-    #             )
-    # print(dnc)
+    for song in drake_songs:
+            # Alt version: 
+            # if float(song[-1]) >= 0.6
+            if float(line["danceability"]) >= 0.6:
+               print(song)
                 
